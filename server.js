51055f8eaae60e1ec2df6f8432e6a125ad44a9b1
rashmika-rajeev <rajeevrashmika@gmail.com>
2024-03-22 18:21:22 +0530

    code added

---
+++ server.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const cors=require('cors');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+const colors=require('colors');
+const connectDB=require("./config/connectDB.js")
+const path = require('path')
+dotenv.config();
+
+connectDB();
+const app = express()
+
+app.use(morgan())
+app.use(express.json())
+app.use(cors())
+
+//routes
+//user routes
+app.use("/api/v1/users", require("./routes/userRoute"));
+
+//transaction routes
+app.use('/api/v1/transactions',require('./routes/transactionRoutes'));
+
+//static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+const PORT= 8080 || process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
